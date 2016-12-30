8ae12c8b1345c4f9007bf1f7038790b0b818b1fe
Egor Suvorov <egor.suvorov@gmail.com>
2016-12-31 02:46:27 +0300

    Add ChatPanel in full, stub for Netchat (bootstrap class)

---
+++ src/main/kotlin/net/yeputons/spbau/fall2016/netchat/Netchat.kt
@@ -0,0 +1,15 @@
+package net.yeputons.spbau.fall2016.netchat
+
+import javax.swing.JFrame
+
+fun main(args: Array<String>) {
+    val config = ChatClientConfiguration("localhost", 12345)
+    val controller = ChatControllerFactory.connect(config)
+    val panel = ChatPanel(controller)
+
+    val frame = JFrame("Netchat")
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    frame.add(panel)
+    frame.pack()
+    frame.isVisible = true
+}
