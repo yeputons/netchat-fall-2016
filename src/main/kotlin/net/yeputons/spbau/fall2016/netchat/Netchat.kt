966b26a8d6bfa60ce96781c2e81e8190b57c0882
Egor Suvorov <egor.suvorov@gmail.com>
2016-12-31 03:13:48 +0300

    Netchat: add 'waiting for connection' frame during connection

--- src/main/kotlin/net/yeputons/spbau/fall2016/netchat/Netchat.kt
+++ src/main/kotlin/net/yeputons/spbau/fall2016/netchat/Netchat.kt
@@ -1,7 +1,9 @@
 package net.yeputons.spbau.fall2016.netchat
 
+import java.awt.BorderLayout
+import java.awt.Font
 import javax.swing.JFrame
-import javax.swing.JOptionPane
+import javax.swing.JLabel
 
 fun main(args: Array<String>) {
     val config = ChatConfigurationDialog.showConfigurationDialog()
@@ -10,8 +12,17 @@ fun main(args: Array<String>) {
         return
     }
 
+    val waitingFrame = JFrame("Netchat waiting")
+    val waitingLabel = JLabel("Waiting for connection...")
+    waitingLabel.font = Font("Arial", Font.PLAIN, 30)
+    waitingFrame.add(waitingLabel, BorderLayout.CENTER)
+    waitingFrame.pack()
+    waitingFrame.isResizable = false
+    waitingFrame.isVisible = true
+
     val controller = ChatControllerFactory.connect(config)
     val panel = ChatPanel(controller)
+    waitingFrame.isVisible = false
 
     val frame = JFrame("Netchat")
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
